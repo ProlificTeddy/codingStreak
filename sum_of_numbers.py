5e9f8fb40b6e0cc8eb75f47219eef0d634883419
ProlificTeddy <162717458+ProlificTeddy@users.noreply.github.com>
2024-12-18 20:54:21 +0300

    Create sum_of_numbers.py

---
+++ sum_of_numbers.py
@@ -0,0 +1,32 @@
+"""Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+Examples (a, b) --> output (explanation)
+
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+Your function should only return a number, not the explanation about how you get that number.
+"""
+def get_sum(a, b):
+    if a > b:
+        a, b = b, a
+    
+
+    total_sum = 0
+    for i in range(a, b + 1):
+        total_sum += i
+    
+    return total_sum
+
+
+print(get_sum(1, 0))   
+print(get_sum(1, 2)) 
+print(get_sum(0, 1))  
+print(get_sum(1, 1))  
+print(get_sum(-1, 0))  
+print(get_sum(-1, 0)) 
